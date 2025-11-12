cecf95c9b23efc4b86b94cb526135e66ed7a243a
Clay1999 <Onischuk18@gmail.com>
2025-11-11 16:38:49 -0800

    Beginning of Hashmaps
    updated User.h and User.cpp
    Registration and Retrieval complete other than main.
    Added optional operations that is not my code

--- User.h
+++ User.h
@@ -14,3 +14,15 @@ public:
     std::string get_email();
     std::string get_name();
 };
+
+//Stores student/tutor objects
+extern std::unordered_map<std::string, std::unique_ptr<User>> userDatabase;
+
+// Insertion for user registration
+void registerNewUser(std::unique_ptr<User> newUser);
+
+//Retrieval
+User* getUserProfile(const std::string& email);
+
+// Update tutor subjects
+// void updateTutorSubjects(const std::string& email, const std::vector<std::string>& newSubjects);
